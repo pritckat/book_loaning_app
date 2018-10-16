ca15415c59aeb59395b7f1160f7e7c605b3d6d85
Katrina <pritckat@gmail.com>
2018-10-16 17:05:43 -0500

    if user is not created, render new user form

--- book_loaning_app/app/controllers/users_controller.rb
+++ book_loaning_app/app/controllers/users_controller.rb
@@ -5,8 +5,12 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        redirect_to user_path(@user)
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def show
