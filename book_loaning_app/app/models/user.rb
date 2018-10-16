3843f16401b1fcf60a3200fd46a36721a8ca2f9e
Katrina <pritckat@gmail.com>
2018-10-16 18:46:53 -0500

    add password length, username uniqueness validations for user class

--- book_loaning_app/app/models/user.rb
+++ book_loaning_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: {minimum: 6, too_short: " must be at least 6 characters" }
 end
