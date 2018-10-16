199f29965fefb4f3a999c7554557fc90726ebbff
Katrina <pritckat@gmail.com>
2018-10-16 15:56:12 -0500

    create new, create user routes

--- book_loaning_app/app/controllers/users_controller.rb
+++ book_loaning_app/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create[username: params[:user][:username], password: params[:user][:password], password_confirmation: [:user][:password_confirmation]]
+        redirect to new_user_path
+    end
 end
