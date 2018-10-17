c83c87ec2685550ae2f5df00f8404011d770930a
Katrina <pritckat@gmail.com>
2018-10-17 13:50:56 -0500

    user can have many books

--- book_loaning_app/app/models/user.rb
+++ book_loaning_app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :books
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :password, length: {minimum: 6, too_short: " must be at least 6 characters" }
