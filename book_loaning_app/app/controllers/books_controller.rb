4b65d1e9b82848258301f9df165079f475deaa20
Katrina <pritckat@gmail.com>
2018-10-17 13:54:26 -0500

    add cr to books controller

--- book_loaning_app/app/controllers/books_controller.rb
+++ book_loaning_app/app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.create(book_params)
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :description)
 end
