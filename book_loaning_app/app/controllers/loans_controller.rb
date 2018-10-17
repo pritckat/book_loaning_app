32adbbd1106f058bdf257efd6d1f2500d9470518
Katrina <pritckat@gmail.com>
2018-10-17 16:45:12 -0500

    can create a book loan

--- book_loaning_app/app/controllers/loans_controller.rb
+++ book_loaning_app/app/controllers/loans_controller.rb
@@ -5,13 +5,18 @@ class LoansController < ApplicationController
     end
 
     def create
-        @loan = Loan.create(loan_params)
-        redirect_to book_path(@loan.book)
+        @loan = Loan.new(loan_params)
+        @book = Book.find_by(title: "Pup's Book")
+        @loan.owner_id = current_user.id
+        @loan.book_id = @book.id
+        @loan.save
+        #raise params
+        redirect_to book_path(@book)
     end
 
     private
     
     def loan_params
-        params.require(:loan).permit()
+        params.require(:loan).permit(:return_datetime, :borrower_id)
     end
 end
