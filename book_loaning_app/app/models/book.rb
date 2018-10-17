334a78cf0a76a656daefeb898d2319a9f50825d6
Katrina <pritckat@gmail.com>
2018-10-17 18:35:02 -0500

    create method to tell if book is loaned or not

--- book_loaning_app/app/models/book.rb
+++ book_loaning_app/app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
     belongs_to :user, optional: true
     has_many :loans
+
+    def loaned?
+        Loan.where(book_id: self.id) != [] ? true : false
+    end
+
 end
