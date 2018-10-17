f66dd368c1bf81ffec5ac62e7c8f856214ccaf2b
Katrina <pritckat@gmail.com>
2018-10-17 14:39:01 -0500

    can update books

--- book_loaning_app/app/controllers/application_controller.rb
+++ book_loaning_app/app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
     private
 
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
     def require_login
         redirect_to login_path unless session.include?:user_id
     end
--- book_loaning_app/app/controllers/books_controller.rb
+++ book_loaning_app/app/controllers/books_controller.rb
@@ -5,13 +5,24 @@ class BooksController < ApplicationController
     end
 
     def create
-        @book = Book.create(book_params)
-        @book.user = User.find(5)
+        @book = Book.new(book_params)
+        @book.user = current_user
+        @book.save
         redirect_to book_path(@book)
     end
 
     def show
         @book = Book.find(params[:id])
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        @book.update(book_params)
+        redirect_to book_path(@book)
     end
 
 
