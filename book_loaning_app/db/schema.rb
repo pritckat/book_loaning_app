722627638026111eca4e1d9594a774140d36bba3
Katrina <pritckat@gmail.com>
2018-10-17 13:47:45 -0500

    create book resource

--- book_loaning_app/db/schema.rb
+++ book_loaning_app/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_193754) do
+ActiveRecord::Schema.define(version: 2018_10_17_184532) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
