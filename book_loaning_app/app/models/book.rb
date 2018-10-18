2facec9e0b9e879a4e44b9ba80df5f5a0ca02645
Katrina <pritckat@gmail.com>
2018-10-17 19:10:23 -0500

    create currently_loaned_to, books return name of user they're loaned to

--- book_loaning_app/app/models/book.rb
+++ book_loaning_app/app/models/book.rb
@@ -2,8 +2,22 @@ class Book < ApplicationRecord
     belongs_to :user, optional: true
     has_many :loans
 
+    
+    
+    def loaned_to 
+        Loan.where(book_id: self.id, returned: false)
+    end
+    
     def loaned?
-        Loan.where(book_id: self.id) != [] ? true : false
+        loaned_to != [] ? true : false
     end
 
+    def currently_loaned_to
+        if self.loaned?
+            borrower_id = self.loaned_to.pluck(:borrower_id)[0]
+            borrower = User.find(borrower_id)
+            borrower.username
+        end
+
+    end
 end
