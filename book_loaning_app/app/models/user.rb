62aeaaf87a752a557e300deeddc0109cafff10f5
Katrina <pritckat@gmail.com>
2018-10-19 18:21:57 -0500

    cannot loan book to self

--- book_loaning_app/app/models/user.rb
+++ book_loaning_app/app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
     
     validates :username, presence: true, uniqueness: true
     validates :password, length: {minimum: 6, too_short: " must be at least 6 characters" }
+
+
+
 end
