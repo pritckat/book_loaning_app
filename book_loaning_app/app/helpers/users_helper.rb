954b74a35d8cc4ba3161dd13ce616c01cf034648
Katrina <pritckat@gmail.com>
2018-10-23 14:31:43 -0500

    change user header

--- book_loaning_app/app/helpers/users_helper.rb
+++ book_loaning_app/app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+    def user_header
+        if User.find(params[:id]) == current_user
+           "Welcome #{current_user.username}!"
+        else
+            user = User.find(params[:id])
+            "#{user.username}'s Page"
+        end
+    end
 end
