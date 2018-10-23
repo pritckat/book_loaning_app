58e3d3212d37eb66af7b95e428c14a80f2d3852b
Katrina <pritckat@gmail.com>
2018-10-23 09:39:59 -0500

    create author index page

--- book_loaning_app/app/controllers/authors_controller.rb
+++ book_loaning_app/app/controllers/authors_controller.rb
@@ -1,2 +1,12 @@
 class AuthorsController < ApplicationController
+
+
+    def index
+        @authors = Author.all 
+    end
+
+    def show
+        @author = Author.find(params[:id])
+    end
+    
 end
