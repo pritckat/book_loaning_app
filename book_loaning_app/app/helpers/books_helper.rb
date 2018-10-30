bb4db5863c44b2ca815505d158bf237fca3daaed
Katrina <pritckat@gmail.com>
2018-10-29 20:45:20 -0500

    edit book loan links helper method

--- book_loaning_app/app/helpers/books_helper.rb
+++ book_loaning_app/app/helpers/books_helper.rb
@@ -1,16 +1,13 @@
 module BooksHelper
 
     def book_loan_links
-        @book = Book.find(params[:id])
         if @book.loaned? && @book.currently_loaned_to != current_user.username
             link_to "Loaned to #{@book.currently_loaned_to}", user_path(User.find_by(username: @book.currently_loaned_to))
         elsif @book.currently_loaned_to == current_user.username
             link_to "Return Book", return_book_path(@book)
-        elsif !@book.loaned?
+        elsif !@book.loaned? && @book.user == current_user
             link_to "Loan Book", new_book_loan_path(@book)
         end
     end
 
-    def user_book_links
-        @book = Book.find(param[:id])
 end
