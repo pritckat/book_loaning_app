5492d17cdc88b807165ac74efe9868e1f06caecb
Katrina <pritckat@gmail.com>
2018-10-29 20:47:20 -0500

    reformat users helper

--- book_loaning_app/app/helpers/users_helper.rb
+++ book_loaning_app/app/helpers/users_helper.rb
@@ -1,11 +1,10 @@
 module UsersHelper
 
     def user_header
-        if User.find(params[:id]) == current_user
-           "Welcome #{current_user.username}!"
+        if @user == current_user
+           "Welcome #{@user.username}!"
         else
-            user = User.find(params[:id])
-            "#{user.username}'s Page"
+            "#{@user.username}'s Page"
         end
     end
 end
