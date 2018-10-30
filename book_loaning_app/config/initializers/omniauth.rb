d19e1fbff2fef6408fb7973e10f01e9a23571e93
Katrina <pritckat@gmail.com>
2018-10-29 20:57:32 -0500

    add dotenv gem for omniauth secrets

--- book_loaning_app/config/initializers/omniauth.rb
+++ book_loaning_app/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-    provider :github, '8c62a79b86e50d97e1e9', 'd7be4008d3258559ade557395ead235939a8c064'
+    provider :github, ENV['secret_thing'], ENV['other_secret']
 end
