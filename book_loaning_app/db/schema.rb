125fb330475567bf7fda072461d42dae4baf9d90
Katrina <pritckat@gmail.com>
2018-11-03 14:47:22 -0500

    add comments to db schema

--- book_loaning_app/db/schema.rb
+++ book_loaning_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_192214) do
+ActiveRecord::Schema.define(version: 2018_11_01_184538) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2018_10_23_192214) do
     t.integer "author_id"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
