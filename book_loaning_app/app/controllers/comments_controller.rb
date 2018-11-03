b1f55033ef7675e49dfdf6fd649df74c0329c17d
Katrina <pritckat@gmail.com>
2018-11-03 14:52:55 -0500

    add create to comments controller

--- book_loaning_app/app/controllers/comments_controller.rb
+++ book_loaning_app/app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            render book_path(@book)
+        else
+            render new_comment_path(@comment)
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:book_id, :user_id, :title, :body)
+    end
 end
