712711e2ce51f4f5d68f52de10f661a49c549509
Katrina <pritckat@gmail.com>
2018-11-05 17:14:53 -0600

    change var's in users.js to let's

--- book_loaning_app/app/assets/javascripts/users.js
+++ book_loaning_app/app/assets/javascripts/users.js
@@ -1,12 +1,12 @@
 $(document).on('turbolinks:load', function(){
     $("a.load_books").on("click", function(e){
-        var $title = $("div.all_books")
+        let $title = $("div.all_books")
         $title.html("<ol></ol>")
         $title.prepend("<h3>Books</h3>")
         //debugger
         $.get(this.href + ".json").success(function(json){
             //debugger
-            var $ol = $("div.all_books ol")
+            let $ol = $("div.all_books ol")
             $ol.html("")
             json.forEach(function(book){
                 //debugger
