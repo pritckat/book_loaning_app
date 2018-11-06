82ae11eabb9edbdaf24bc9ae89273bce446089b2
Katrina <pritckat@gmail.com>
2018-11-06 11:47:09 -0600

    add previous button to comment show page

--- book_loaning_app/app/assets/javascripts/comments.js
+++ book_loaning_app/app/assets/javascripts/comments.js
@@ -13,4 +13,18 @@ $(document).on('turbolinks:load', function(){
             $(".comment_body").text(data["body"])
         })
     })
+
+    $("a.js-prev").on("click", function(e){
+        e.preventDefault()
+        let nextId = parseInt($(".js-next").attr("data-commentid")) - 1;
+        const bookId = parseInt($(".js-next").attr("data-bookid"))
+        $.get("/books/" + bookId + "/comments/" + nextId +".json", function(data) {
+            console.log(data)
+            $(".comment_title").text(data["title"]);
+            $(".js-next").attr("data-commentid", data["id"])
+            $(".comment_user").text(data["user"]["username"])
+            $(".comment_book").text(data["book"]["title"])
+            $(".comment_body").text(data["body"])
+        })
+    })
 })
