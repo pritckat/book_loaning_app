e694158da0fd3fea0294aff4e3a093b296ea5984
Katrina <pritckat@gmail.com>
2018-11-05 21:30:13 -0600

    add reply associations

--- book_loaning_app/app/models/user.rb
+++ book_loaning_app/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_many :loans, through: :books
     has_many :authors, through: :books
     has_many :comments
+    has_many :replies
     
     validates :username, presence: true, uniqueness: true
     validates :password, length: {minimum: 6, too_short: " must be at least 6 characters" }
