8b2aa39ab956e88ee9e09d9ec6104ac59f091043
Katrina <pritckat@gmail.com>
2018-11-05 21:10:31 -0600

    can cycle through comments show pages with next link

--- book_loaning_app/app/assets/javascripts/comments.js
+++ book_loaning_app/app/assets/javascripts/comments.js
@@ -2,7 +2,13 @@ $(document).on('turbolinks:load', function(){
 
     $("a.js-next").on("click", function(e){
         e.preventDefault()
-        alert("hello")
+        let nextId = parseInt($(".js-next").attr("data-commentid")) + 1;
+        const bookId = parseInt($(".js-next").attr("data-bookid"))
+        $.get("/books/" + bookId + "/comments/" + nextId +".json", function(data) {
+            console.log(data)
+            $(".comment_title").text(data["title"]);
+            $(".js-next").attr("data-commentid", data["id"])
+            console.log(".js-next")
+        })
     })
-    alert("hello")
 })
