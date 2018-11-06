6355634cc3450f28eb2776794cf055c4030e1dbe
Katrina <pritckat@gmail.com>
2018-11-06 12:16:02 -0600

    refactor comment.js

--- book_loaning_app/app/assets/javascripts/comments.js
+++ book_loaning_app/app/assets/javascripts/comments.js
@@ -1,30 +1,38 @@
 $(document).on('turbolinks:load', function(){
 
     $("a.js-next").on("click", function(e){
-        e.preventDefault()
-        let nextId = parseInt($(".js-next").attr("data-commentid")) + 1;
-        const bookId = parseInt($(".js-next").attr("data-bookid"))
-        $.get("/books/" + bookId + "/comments/" + nextId +".json", function(data) {
-            console.log(data)
-            $(".comment_title").text(data["title"]);
-            $(".js-next").attr("data-commentid", data["id"])
-            $(".comment_user").text(data["user"]["username"])
-            $(".comment_book").text(data["book"]["title"])
-            $(".comment_body").text(data["body"])
-        })
+        nextComment(e)
     })
 
-    $("a.js-prev").on("click", function(e){
-        e.preventDefault()
-        let nextId = parseInt($(".js-next").attr("data-commentid")) - 1;
-        const bookId = parseInt($(".js-next").attr("data-bookid"))
-        $.get("/books/" + bookId + "/comments/" + nextId +".json", function(data) {
-            console.log(data)
-            $(".comment_title").text(data["title"]);
-            $(".js-next").attr("data-commentid", data["id"])
-            $(".comment_user").text(data["user"]["username"])
-            $(".comment_book").text(data["book"]["title"])
-            $(".comment_body").text(data["body"])
-        })
+    $("a.js-prev").on("click", function(e) {
+        prevComment(e)
     })
 })
+
+const bookId = parseInt($(".js-next").attr("data-bookid"))
+
+const showComment = (data) => {
+    $(".comment_title").text(data["title"]);
+    $(".js-next").attr("data-commentid", data["id"])
+    $(".comment_user").text(data["user"]["username"])
+    $(".comment_book").text(data["book"]["title"])
+    $(".comment_body").text(data["body"])
+}
+
+const prevComment = function(e) {
+    e.preventDefault()
+    let nextId = parseInt($(".js-next").attr("data-commentid")) - 1;
+    $.get("/books/" + bookId + "/comments/" + nextId +".json", function(data) {
+        console.log(data)
+        showComment(data)
+    })
+}
+
+const nextComment = function(e) {
+    e.preventDefault()
+    let nextId = parseInt($(".js-next").attr("data-commentid")) + 1;
+    $.get("/books/" + bookId + "/comments/" + nextId +".json", function(data) {
+        console.log(data)
+        showComment(data)
+    })
+}
