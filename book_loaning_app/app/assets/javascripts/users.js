15c9aa47f52eb40a6af9e0e96f7e492695a0c3f3
Katrina <pritckat@gmail.com>
2018-11-08 10:44:56 -0600

    turn off javascript event for All Books link on user show page

--- book_loaning_app/app/assets/javascripts/users.js
+++ book_loaning_app/app/assets/javascripts/users.js
@@ -1,18 +1,18 @@
-$(document).on('turbolinks:load', function(){
-    $("a.load_books").on("click", function(e){
-        let $title = $("div.all_books")
-        $title.html("<ol></ol>")
-        $title.prepend("<h3>Books</h3>")
-        //debugger
-        $.get(this.href + ".json").success(function(json){
-            //debugger
-            let $ol = $("div.all_books ol")
-            $ol.html("")
-            json.forEach(function(book){
-                //debugger
-                $ol.append("<li>" +`<a href="/books/${book.id}">` + book.title + "</li>")
-            })
-        })
-        e.preventDefault();
-    })
-})
+// $(document).on('turbolinks:load', function(){
+//     $("a.load_books").on("click", function(e){
+//         let $title = $("div.all_books")
+//         $title.html("<ol></ol>")
+//         $title.prepend("<h3>Books</h3>")
+//         //debugger
+//         $.get(this.href + ".json").success(function(json){
+//             //debugger
+//             let $ol = $("div.all_books ol")
+//             $ol.html("")
+//             json.forEach(function(book){
+//                 //debugger
+//                 $ol.append("<li>" +`<a href="/books/${book.id}">` + book.title + "</li>")
+//             })
+//         })
+//         e.preventDefault();
+//     })
+// })
